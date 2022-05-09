ff23e0d1175e4fd34863f8b05bb919b59ac3630e
Jason <jlee220@gmail.com>
2022-05-09 16:38:24 -0700

    created sales models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class VehicleModelVO(models.Model):
+    name = models.CharField(max_length=100)
+    import_href = models.CharField(max_length=200, unique=True)
+ 
+    def __str__(self):
+        return self.name 
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Sale(models.Model):    
+    sales_person = models.CharField(max_length=200)
+    employee_number = models.CharField(max_length=100, unique=True)
+    sale_price = models.IntegerField()
+
+    automobile = models.ForeignKey(
+        VehicleModelVO,
+        related_name="automobile",
+        on_delete=models.CASCADE,
+    )
+
+    pot_customer = models.ForeignKey(
+        Customer,
+        related_name="customer",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f'{self.sales_person}, {self.automobile}'
---
+++ sales/api/sales_rest/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+# from .views import list_sales, show_shoe
+
+urlpatterns = [
+    # path("sales/", list_sales, name="list_sales"),    
+    # path("sales/<int:pk>/", show_shoe, name="show_shoe"),
+    
+]
