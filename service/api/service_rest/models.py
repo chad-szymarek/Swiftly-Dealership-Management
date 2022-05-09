7e7b825326a733016f2fd9db6e19964ed29a6ea0
chad.szymarek <chadszymarek@gmail.com>
2022-05-09 16:34:46 -0600

    made get and post views for technicians

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -6,9 +6,18 @@ class Technician(models.Model):
     name = models.CharField(max_length=300)
     employee_number = models.PositiveBigIntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Appointment(models.Model):
     owner = models.CharField(max_length=200)
     vin = models.CharField(max_length=300)
     date_time = models.DateTimeField()
-    technician = models.ManyToManyField("Technician", related_name="appointment", on_delete=models.PROTECT)
+    technician = models.ForeignKey("Technician", related_name="appointment", on_delete=models.PROTECT)
     reason = models.TextField()
+
+    def __str__(self):
+        return self.owner
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=300)
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
+from django.views.decorators.http import require_http_methods
+
+from .models import Technician, Appointment
+from common.json import ModelEncoder
 
 # Create your views here.
+
+class TechnicianListEncoder(ModelEncoder):
+    model = Technician
+    properties = ["name", "employee_number"]
+
+@require_http_methods(["GET", "POST"])
+def api_list_technicians(request):
+    if request.method == "GET":
+        technicians = Technician.objects.all()
+
+        return JsonResponse(
+            {"technicians": technicians},
+            encoder=TechnicianListEncoder
+        )
+    else:
+        content = json.loads(request.body)
+        technician = Technician.objects.create(**content)
+        return JsonResponse(
+            technician,
+            encoder=TechnicianListEncoder,
+            safe=False,
+        )
