d402d6fc840d9bc680301895388a5973fcff7fa9
chad.szymarek <chadszymarek@gmail.com>
2022-05-09 15:52:43 -0600

    added service models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Technician(models.Model):
+    name = models.CharField(max_length=300)
+    employee_number = models.PositiveBigIntegerField()
+
+class Appointment(models.Model):
+    owner = models.CharField(max_length=200)
+    vin = models.CharField(max_length=300)
+    date_time = models.DateTimeField()
+    technician = models.ManyToManyField("Technician", related_name="appointment", on_delete=models.PROTECT)
+    reason = models.TextField()
