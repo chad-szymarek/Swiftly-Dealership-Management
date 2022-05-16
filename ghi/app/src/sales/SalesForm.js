35aab9dc48679d92589340bcbad7da3e26f918d4
chad.szymarek <chadszymarek@gmail.com>
2022-05-16 15:51:35 -0600

    final commit

--- ghi/app/src/sales/SalesForm.js
+++ ghi/app/src/sales/SalesForm.js
@@ -50,7 +50,7 @@ class SalesForm extends React.Component {
         accum[soldCar.automobile] = true;
         return accum;
       }, {});
-      const filteredCars = notSold.filter((unsold) => !soldVins[unsold.vin]);
+      const filteredCars = notSold.filter(unsold => !soldVins[unsold.vin]);
 
       this.setState({
         salesPersons: saleData.salespersons,
@@ -112,34 +112,34 @@ class SalesForm extends React.Component {
       rtMsg = "alert alert-secondary mb-3";
       formClasses = "d-none";
       makeNew = "btn btn-primary btn-lg active mb-3";
-      countDown = <Timer count={10} />;
+      countDown = <Timer count={15} />;
     }
 
     if (this.state.navigate) {
-      return <Navigate to="/sales/list" />;
+      return <Navigate to='/sales/list' />;
     }
 
     return (
-      <div className="row">
-        <div className="offset-3 col-6">
-          <div className="shadow p-4 mt-4">
+      <div className='row'>
+        <div className='offset-3 col-6'>
+          <div className='shadow p-4 mt-4'>
             <h1>Record a new sale</h1>
             <form
               onSubmit={this.handleSubmit}
               className={formClasses}
-              id="create-sales-form"
+              id='create-sales-form'
             >
-              <div className="mb-3">
+              <div className='mb-3'>
                 <select
                   onChange={this.handleChange}
                   value={this.state.automobile}
                   required
-                  name="automobile"
-                  id="automobile"
-                  className="form-select"
+                  name='automobile'
+                  id='automobile'
+                  className='form-select'
                 >
-                  <option value="">Choose an automobile</option>
-                  {this.state.autos.map((auto) => {
+                  <option value=''>Choose an automobile</option>
+                  {this.state.autos.map(auto => {
                     return (
                       <option key={auto.href} value={auto.vin}>
                         {auto.year} {auto.color} {auto.model.manufacturer.name}{" "}
@@ -149,17 +149,17 @@ class SalesForm extends React.Component {
                   })}
                 </select>
               </div>
-              <div className="mb-3">
+              <div className='mb-3'>
                 <select
                   onChange={this.handleChange}
                   value={this.state.salesPerson}
                   required
-                  name="salesPerson"
-                  id="salesPerson"
-                  className="form-select"
+                  name='salesPerson'
+                  id='salesPerson'
+                  className='form-select'
                 >
-                  <option value="">Choose a sales person</option>
-                  {this.state.salesPersons.map((salesperson) => {
+                  <option value=''>Choose a sales person</option>
+                  {this.state.salesPersons.map(salesperson => {
                     return (
                       <option key={salesperson.name} value={salesperson.name}>
                         {salesperson.name}
@@ -168,18 +168,18 @@ class SalesForm extends React.Component {
                   })}
                 </select>
               </div>
-              <div className="mb-3">
+              <div className='mb-3'>
                 <select
                   onChange={this.handleChange}
                   value={this.state.customer}
                   required
-                  name="customer"
-                  id="customer"
-                  className="form-select"
+                  name='customer'
+                  id='customer'
+                  className='form-select'
                 >
-                  <option value="">Choose a customer</option>
+                  <option value=''>Choose a customer</option>
 
-                  {this.state.customers.map((customer) => {
+                  {this.state.customers.map(customer => {
                     return (
                       <option key={customer.id} value={customer.name}>
                         {customer.name}
@@ -188,30 +188,30 @@ class SalesForm extends React.Component {
                   })}
                 </select>
               </div>
-              <div className="form-floating mb-3">
+              <div className='form-floating mb-3'>
                 <input
                   onChange={this.handleChange}
                   value={this.state.price}
-                  placeholder="Sale price"
+                  placeholder='Sale price'
                   required
-                  type="number"
-                  name="price"
-                  id="price"
-                  className="form-control"
+                  type='number'
+                  name='price'
+                  id='price'
+                  className='form-control'
                 />
-                <label htmlFor="price">Sale price</label>
+                <label htmlFor='price'>Sale price</label>
               </div>
-              <button className="btn btn-primary">Create</button>
+              <button className='btn btn-primary'>Create</button>
             </form>
-            <div className={messageClasses} id="success-message">
+            <div className={messageClasses} id='success-message'>
               You've submitted a sales record!
             </div>
-            <div className="d-flex justify-content-center">
+            <div className='d-flex justify-content-center'>
               <a
-                href="/sales"
+                href='/sales'
                 className={makeNew}
-                role="button"
-                aria-pressed="true"
+                role='button'
+                aria-pressed='true'
               >
                 Add Another Record
               </a>
