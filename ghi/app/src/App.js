884c788c8fea71cf9c6c91425f5368866529c6c3
chad.szymarek <chadszymarek@gmail.com>
2022-05-18 17:28:55 -0600

    commiting before merge

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useContext, useState } from "react";
 
 import MainPage from "./MainPage";
 import Nav from "./Nav";
@@ -20,59 +21,56 @@ import CustomerForm from "./sales/CustomerForm";
 import SalesForm from "./sales/SalesForm";
 import SalesList from "./sales/SalesList";
 import SalesHistoryList from "./sales/SalesHistoryList";
-import "./App.css";
 
 function App(props) {
   return (
     <BrowserRouter>
       <Nav />
-      <div className='container'>
-        <Routes>
-          <Route path='/' element={<MainPage />} />
-          <Route path='appointments'>
-            <Route index element={<AppointmentList />} />
-            <Route path='create/' element={<AppointmentForm />} />
-            <Route path='history/' element={<AppointmentHistory />} />
-          </Route>
-          <Route path='technicians'>
-            <Route index element={<TechnicianForm />} />
-          </Route>
-          <Route path='manufacturers'>
-            <Route index element={<ManufacturerList />} />
-            <Route path='create/' element={<ManufacturerForm />} />
-          </Route>
-          <Route path='automobiles'>
-            <Route index element={<AutomobileList />} />
-            <Route path='create' element={<AutomobileForm />} />
-          </Route>
-          <Route path='vehicles'>
-            <Route index element={<VehicleList />} />
-            <Route path='create' element={<VehicleForm />} />
-          </Route>
-          <Route path='salesperson'>
-            <Route index element={<SalespersonForm />} />
-          </Route>
-          <Route path='customer'>
-            <Route path='' element={<CustomerForm />} />
-          </Route>
-          <Route path='sales'>
-            <Route path='' element={<SalesForm />} />
-            <Route
-              path='list'
-              element={<SalesList salespersons={props.salespersons} />}
-            />
-            <Route
-              path='history'
-              element={
-                <SalesHistoryList
-                  salespersons={props.salespersons}
-                  salesreps={props.salesreps}
-                />
-              }
-            />
-          </Route>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='appointments'>
+          <Route index element={<AppointmentList />} />
+          <Route path='create/' element={<AppointmentForm />} />
+          <Route path='history/' element={<AppointmentHistory />} />
+        </Route>
+        <Route path='technicians'>
+          <Route index element={<TechnicianForm />} />
+        </Route>
+        <Route path='manufacturers'>
+          <Route index element={<ManufacturerList />} />
+          <Route path='create/' element={<ManufacturerForm />} />
+        </Route>
+        <Route path='automobiles'>
+          <Route index element={<AutomobileList />} />
+          <Route path='create' element={<AutomobileForm />} />
+        </Route>
+        <Route path='vehicles'>
+          <Route index element={<VehicleList />} />
+          <Route path='create' element={<VehicleForm />} />
+        </Route>
+        <Route path='salesperson'>
+          <Route index element={<SalespersonForm />} />
+        </Route>
+        <Route path='customer'>
+          <Route path='' element={<CustomerForm />} />
+        </Route>
+        <Route path='sales'>
+          <Route path='' element={<SalesForm />} />
+          <Route
+            path='list'
+            element={<SalesList salespersons={props.salespersons} />}
+          />
+          <Route
+            path='history'
+            element={
+              <SalesHistoryList
+                salespersons={props.salespersons}
+                salesreps={props.salesreps}
+              />
+            }
+          />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
